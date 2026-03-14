4d6e28b4291cf92260204a94cf662ecc9e8b5702
Joshy <145135212+JshyQ@users.noreply.github.com>
2026-03-14 18:16:10 +0700

    Update info.js

--- cmd/command/info.js
+++ cmd/command/info.js
@@ -1,4 +1,447 @@
-ev.on({
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import { execSync } from 'child_process'
+import { performance } from 'perf_hooks'
+import os from 'os'
+
+const BANNER_URL   = 'https://images8.alphacoders.com/584/thumb-1920-584430.jpg'
+const CHANNEL_JID  = '0029VbCWrvqJpe8nXG7Y0S0v@newsletter'
+const CHANNEL_NAME = 'ChappieBot'
+
+export default function info(ev) {
+  ev.on({
+    name: 'afk',
+    cmd: ['afk'],
+    tags: 'Info Menu',
+    desc: 'menandai kamu afk',
+    owner: !1,
+    prefix: !0,
+    money: 1,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      args,
+      chat,
+      cmd
+    }) => {
+      try {
+        const user = Object.values(db().key).find(u => u.jid === chat.sender),
+              reason = args.join(' ') || 'tidak ada alasan',
+              time = global.time.timeIndo('Asia/Jakarta', 'DD-MM HH:mm:ss')
+
+        if (!user) return xp.sendMessage(chat.id, { text: 'kamu belum terdaftar, ulangi' }, { quoted: m })
+
+        user.afk.status = !0
+        user.afk.reason = reason
+        user.afk.afkStart = time
+        save.db()
+
+        await xp.sendMessage(chat.id, { text: `Kamu memulai afk\ndengan alasan: ${reason}` }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'cekcuaca',
+    cmd: ['cekcuaca', 'cuaca'],
+    tags: 'Info Menu',
+    desc: 'info saluran cuaca',
+    owner: !1,
+    prefix: !0,
+    money: 100,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      args,
+      chat,
+      cmd,
+      prefix
+    }) => {
+      try {
+        const kota = args.join(' ')
+        if (!kota) return xp.sendMessage(chat.id, { text: `contoh: ${prefix}${cmd} jakarta` }, { quoted: m })
+
+        const url = await fetch(`https://api.ureshii.my.id/api/internet/info-cuaca?kota=${encodeURIComponent(kota)}`),
+              res = await url.json()
+
+        await xp.sendMessage(chat.id, { react: { text: '⏳', key: m.key } })
+
+        if (!res.kota || !res.cuaca) return xp.sendMessage(chat.id, { text: `gagal mendapatkan info cuaca untuk kota: ${kota}` }, { quoted: m })
+
+        let txt = '*Cuaca Hari Ini Di*\n\n'
+            txt += `${head}${opb} ${res.kota}, ${res.negara} ${clb}\n`
+            txt += `${body} ${btn} *Cuaca:* ${res.cuaca}\n`
+            txt += `${body} ${btn} *Kelembapan:* ${res.kelembapan}%\n`
+            txt += `${body} ${btn} *Kec Angin:* ${res.angin_kpj} m/s\n`
+            txt += `${body} ${btn} *Suhu Saat Ini:* ${res.suhu_c}°\n`
+            txt += `${body} ${btn} *Suhu Tertinggi:* ${res.suhu_tertinggi_c}°\n`
+            txt += `${body} ${btn} *Suhu Terendah:* ${res.suhu_terendah_c}°\n`
+            txt += `${foot}${line}\n`
+            txt += 'Semoga harimu menyenangkan! Jangan lupa bawa payung kalau cuacanya mendung ya! ☂'
+
+        await xp.sendMessage(chat.id, { text: txt }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'cekgc',
+    cmd: ['cekgc'],
+    tags: 'Info Menu',
+    desc: 'mengecek status grup',
+    owner: !1,
+    prefix: !0,
+    money: 100,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      chat,
+      cmd,
+      prefix
+    }) => {
+      try {
+        const gcData = getGc(chat),
+              metadata = groupCache.get(chat.id),
+              name = metadata.subject,
+              member = metadata.participants.length,
+              { usrAdm, botAdm } = await grupify(xp, m),
+              defThumb = 'https://c.termai.cc/i0/7DbG.jpg'
+
+        if (!chat.group || !gcData || !usrAdm || !botAdm) {
+          return xp.sendMessage(chat.id, { text: !chat.group ? 'perintah ini hanya bisa digunakan digrup' : !gcData ? `grup ini belum terdaftar ketik ${prefix}daftargc untuk mendaftar` : !usrAdm ? 'kamu bukan admin' : 'aku bukan admin' }, { quoted: m })
+        }
+
+        let txt = `${head} ${opb} *Informasi Grup* ${clb}\n`
+            txt += `${body} ${btn} *Nama: ${name}*\n`
+            txt += `${body} ${btn} *Id: ${gcData?.id}*\n`
+            txt += `${body} ${btn} *Diban: ${gcData?.ban ? 'Iya' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Member: ${gcData?.member}*\n`
+            txt += `${foot}${line}\n`
+            txt += `${head}${opb} *Pengaturan Grup* ${clb}\n`
+            txt += `${body} ${btn} *Anti Ch: ${gcData?.filter?.antich ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Anti Badword: ${gcData?.filter?.badword?.antibadword ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Anti Link: ${gcData?.filter?.antilink ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Anti TagSw: ${gcData?.filter?.antitagsw ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Leave: ${gcData?.filter?.left?.leftGc ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Welcome: ${gcData?.filter?.welcome?.welcomeGc ? 'Aktif' : 'Tidak'}*\n`
+            txt += `${foot}${line}\n`
+            txt += `${head}${opb} *Blacklist Kata* ${clb}\n`
+            txt += `${body} ${btn} *Kata: ${gcData?.filter?.badword?.badwordtext || '-'}*\n`
+            txt += `${foot}${line}`
+
+        let thumb = await xp.profilePictureUrl(metadata.id, 'image') || defThumb
+
+        await xp.sendMessage(chat.id, {
+          text: txt,
+          contextInfo: {
+            externalAdReply: {
+              body: `Ini adalah informasi grup ${name}`,
+              thumbnailUrl: thumb,
+              mediaType: 1,
+              renderLargerThumbnail: !0,
+              sourceUrl: 'https://whatsapp.com/channel/0029VbCWrvqJpe8nXG7Y0S0v'
+            },
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: CHANNEL_JID,
+              newsletterName: CHANNEL_NAME
+            }
+          }
+        })
+
+        if (gcData.member !== metadata?.participants.length) {
+          gcData.member = metadata.participants.length
+          save.gc()
+        }
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'help',
+    cmd: ['help'],
+    tags: 'Info Menu',
+    desc: 'informasi fitur',
+    owner: !1,
+    prefix: !0,
+    money: 1,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      args,
+      chat,
+      cmd,
+      prefix
+    }) => {
+      try {
+        const text = args[0]?.toLowerCase(),
+              cmdFile = path.join(process.cwd(), 'cmd', 'command')
+
+        if (!text) return xp.sendMessage(chat.id, { text: `gunakan:\n${prefix}${cmd} menu` }, { quoted: m })
+
+        const files = fs.readdirSync(cmdFile).filter(f => f.endsWith('.js'))
+        let found = null
+
+        for (const file of files) {
+          const event = (await import(`file://${path.join(cmdFile, file)}`)).default
+          if (typeof event !== 'function') continue
+
+          event({
+            on: obj => {
+              const name = obj?.name?.toLowerCase(),
+                    cmds = Array.isArray(obj?.cmd) ? obj.cmd.map(v => v.toLowerCase()) : []
+              if (name === text || cmds.includes(text)) found = obj
+            }
+          })
+
+          if (found) break
+        }
+
+        if (!found) return xp.sendMessage(chat.id, { text: `fitur ${text} tidak ada` }, { quoted: m })
+
+        let txt = `${head} ${opb} *I N F O R M A S I* ${clb}\n`
+            txt += `${body} ${btn} *Nama: ${found.name || '-'}*\n`
+            txt += `${body} ${btn} *Cmd: ${Array.isArray(found.cmd) ? found.cmd.map(c => '.' + c).join(', ') : '-'}*\n`
+            txt += `${body} ${btn} *Tags: ${found.tags || '-'}*\n`
+            txt += `${body} ${btn} *Deskripsi: ${found.desc || '-'}*\n`
+            txt += `${body} ${btn} *Owner Only: ${found.owner ? 'Ya' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Prefix: ${found.prefix ? 'Ya' : 'Tidak'}*\n`
+            txt += `${body} ${btn} *Pajak: Rp ${found.money.toLocaleString('id-ID') || 0}*\n`
+            txt += `${body} ${btn} *Exp: ${found.exp || 0.1}*\n`
+            txt += `${foot}${line}`
+
+        await xp.sendMessage(chat.id, {
+          text: txt,
+          contextInfo: {
+            externalAdReply: {
+              body: `informasi fitur ${found.name}`,
+              thumbnailUrl: BANNER_URL,
+              mediaType: 1,
+              renderLargerThumbnail: !0,
+              sourceUrl: 'https://whatsapp.com/channel/0029VbCWrvqJpe8nXG7Y0S0v'
+            },
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: CHANNEL_JID,
+              newsletterName: CHANNEL_NAME
+            }
+          }
+        }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'menu',
+    cmd: ['menu'],
+    tags: 'Info Menu',
+    desc: 'main Menu',
+    owner: !1,
+    prefix: !0,
+    money: 1,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      args,
+      chat,
+      cmd
+    }) => {
+      try {
+        const time = global.time.timeIndo('Asia/Jakarta', 'HH:mm'),
+              filterTag = args[0]?.toLowerCase(),
+              cmds = ev.cmd || [],
+              name = chat.pushName,
+              commands = {},
+              allUsr = Object.keys(db().key).length
+
+        for (const c of cmds) {
+          const tag = c.tags || 'Other',
+                name = c.name || (Array.isArray(c.cmd) ? c.cmd[0] : c.cmd)
+          commands[tag] ??= []
+          name && commands[tag].push(name)
+        }
+
+        const allCmd = Object.values(commands).reduce((a, b) => a + b.length, 0)
+
+        let txt =
+          `Halo *${name}*, Saya adalah asisten virtual.\n\n` +
+          `${head}${opb} *${botName}* ${clb}\n` +
+          `${body} ${btn} *Bot Name: ${botFullName}*\n` +
+          `${body} ${btn} *Owner: ${ownerName}*\n` +
+          `${body} ${btn} *Waktu: ${time}*\n` +
+          `${body} ${btn} *All Cmd: ${allCmd}*\n` +
+          `${body} ${btn} *Total User: ${allUsr}*\n` +
+          `${foot}${line}\n${readmore}\n`
+
+        const entries = (filterTag
+          ? Object.entries(commands).filter(([cat]) => cat.toLowerCase().includes(filterTag))
+          : Object.entries(commands)
+        ).sort(([a], [b]) => a.localeCompare(b))
+
+        !entries.length
+          ? txt += `${body} Tag *${filterTag}* tidak ditemukan!\n`
+          : entries.forEach(([cat, features]) => {
+              features.length &&
+              (txt +=
+                `${head}${opb} *${cat.charAt(0).toUpperCase() + cat.slice(1)}* ${clb}\n` +
+                features.map(f => `${body} ${btn} *${f}*`).join('\n') +
+                `\n${foot}${line}\n\n`)
+            })
+
+        txt += `${footer}`
+
+        await xp.sendMessage(chat.id, {
+          text: txt,
+          contextInfo: {
+            externalAdReply: {
+              body: `Ini adalah menu ${botName}`,
+              thumbnailUrl: BANNER_URL,
+              mediaType: 1,
+              renderLargerThumbnail: !0,
+              sourceUrl: 'https://whatsapp.com/channel/0029VbCWrvqJpe8nXG7Y0S0v'
+            },
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: CHANNEL_JID,
+              newsletterName: CHANNEL_NAME
+            }
+          }
+        }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'owner',
+    cmd: ['owner', 'contact'],
+    tags: 'Info Menu',
+    desc: 'menampilkan kontak owner',
+    owner: !1,
+    prefix: !0,
+    money: 100,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      chat,
+      cmd
+    }) => {
+      try {
+        const owner = global.ownerName || 'error',
+              bot = global.botName || 'error',
+              ownerNumber = Array.isArray(global.ownerNumber) ? global.ownerNumber : [global.ownerNumber]
+
+        if (!ownerNumber || !ownerNumber.length) return xp.sendMessage(chat.id, { text: 'tidak ada kontak owner' }, { quoted: m })
+
+        const contact = ownerNumber.map((num, i) => ({ vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${owner} ${i + 1}\nTEL;type=CELL;waid=${num}:${num}\nEND:VCARD` })),
+              displayName = ownerNumber.length > 1 ? `${owner} dan ${ownerNumber.length - 1} lainnya` : owner
+
+        await xp.sendMessage(chat.id, { contacts: { displayName, contacts: contact } }, { quoted: m })
+        await xp.sendMessage(chat.id, { text: 'ini adalah kontak owner ku' }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
+    name: 'profile',
+    cmd: ['profile', 'me'],
+    tags: 'Info Menu',
+    desc: 'mengecek profile orang',
+    owner: !1,
+    prefix: !0,
+    money: 1,
+    exp: 0.1,
+
+    run: async (xp, m, {
+      chat,
+      cmd
+    }) => {
+      try {
+        const data = Object.values(db().key).find(u => u.jid === chat.sender),
+              base64 = v => Buffer.from(v).toString('base64'),
+              defThumb = 'https://c.termai.cc/i0/7DbG.jpg',
+              type = v => v ? 'Aktif' : 'Tidak'
+
+        if (!data) return xp.sendMessage(chat.id, { text: 'kamu belum terdaftar, ulangi' }, { quoted: m })
+
+        let thumb
+        try { thumb = await xp.profilePictureUrl(chat.sender, 'image') }
+        catch { thumb = defThumb }
+
+        const name = chat.pushName || m.pushName,
+              nomor = data.jid,
+              noId = base64(data.noId),
+              cmdCount = data.cmd,
+              ban = type(data.ban),
+              ai = type(data.ai?.bell),
+              farm = type(data?.game?.farm),
+              chatAi = data.ai.chat,
+              role = data.ai.role,
+              acc = data.acc,
+              money = data.moneyDb?.money.toLocaleString('id-ID'),
+              moneyInBank = data.moneyDb?.moneyInBank.toLocaleString('id-ID'),
+              exp = data.exp || 0,
+              level = ((exp + 10) / 10).toFixed(1)
+
+        let txt = `${head} ${opb} *P R O F I L E* ${clb}\n`
+            txt += `${body} ${btn} *Nama:* ${name}\n`
+            txt += `${body} ${btn} *Nomor:* ${nomor}\n`
+            txt += `${body} ${btn} *No ID:* ${noId}\n`
+            txt += `${body} ${btn} *Cmd:* ${cmdCount}\n`
+            txt += `${body} ${btn} *Ban:* ${ban}\n`
+            txt += `${body} ${btn} *Acc:* ${acc}\n`
+            txt += `${foot}${line}\n\n`
+            txt += `${head} ${opb} *G A M E* ${clb}\n`
+            txt += `${body} ${btn} *Money:* Rp ${money}\n`
+            txt += `${body} ${btn} *Uang Di Bank:* Rp ${moneyInBank}\n`
+            txt += `${body} ${btn} *Level:* ${level}\n`
+            txt += `${body} ${btn} *Auto Farm:* ${farm}\n`
+            txt += `${foot}${line}\n\n`
+            txt += `${head} ${opb} *A I* ${clb}\n`
+            txt += `${body} ${btn} *Ai:* ${ai}\n`
+            txt += `${body} ${btn} *Chat Ai:* ${chatAi}\n`
+            txt += `${body} ${btn} *Role:* ${role}\n`
+            txt += `${foot}${line}`
+
+        await xp.sendMessage(chat.id, {
+          text: txt,
+          contextInfo: {
+            externalAdReply: {
+              body: `ini profile ${name}`,
+              thumbnailUrl: thumb,
+              mediaType: 1,
+              renderLargerThumbnail: !0,
+              sourceUrl: 'https://whatsapp.com/channel/0029VbCWrvqJpe8nXG7Y0S0v'
+            },
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: CHANNEL_JID,
+              newsletterName: CHANNEL_NAME
+            }
+          }
+        }, { quoted: m })
+      } catch (e) {
+        err(`error pada ${cmd}`, e)
+        call(xp, e, m)
+      }
+    }
+  })
+
+  ev.on({
     name: 'stats',
     cmd: ['st', 'stats', 'ping'],
     tags: 'Info Menu',
@@ -41,7 +484,6 @@ ev.on({
     ${body} ${btn} *Time:* ${time}
     ${body} ${btn} *Respon:* ${(performance.now() - a).toFixed(2)} ms
     ${foot}${line}
-
     ${head} ${opb} Stats System ${clb}
     ${body} ${btn} *Platform:* ${platform} ( ${arch} )
     ${body} ${btn} *Cpu:* ${cpu}
@@ -50,15 +492,20 @@ ev.on({
     ${foot}${line}`.trim()
 
         await xp.sendMessage(chat.id, {
-          image: { url: 'https://images8.alphacoders.com/584/thumb-1920-584430.jpg' },
+          image: { url: BANNER_URL },
           caption: stats,
           contextInfo: {
             externalAdReply: {
               title: botFullName,
               body: `Ini adalah stats ${botName}`,
-              thumbnailUrl: 'https://images8.alphacoders.com/584/thumb-1920-584430.jpg',
+              thumbnailUrl: BANNER_URL,
               mediaType: 1,
-              renderLargerThumbnail: !0
+              renderLargerThumbnail: !0,
+              sourceUrl: 'https://whatsapp.com/channel/0029VbCWrvqJpe8nXG7Y0S0v'
+            },
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: CHANNEL_JID,
+              newsletterName: CHANNEL_NAME
             }
           }
         }, { quoted: m })
@@ -68,3 +515,4 @@ ev.on({
       }
     }
   })
+}
